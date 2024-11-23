21c2f798c7596bdbc6fd416f73d780fd53f2963c
никита часовких <hasnikita@mail.ru>
2024-11-24 02:59:35 +0300

    class Game

--- main.h
+++ main.h
@@ -1,10 +1,16 @@
 //
 // Created by hasni on 04.10.2024.
 //
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <stack>
+#include "derevo.h"
 #include <torch/torch.h>
+#include "model.cpp"
 #ifndef ALPA_TENSOR_MAIN_H
 #define ALPA_TENSOR_MAIN_H
 namespace MonteKarlo {
-
 };
 #endif //ALPA_TENSOR_MAIN_H
